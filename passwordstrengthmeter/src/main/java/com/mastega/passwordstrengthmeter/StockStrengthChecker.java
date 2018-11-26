2d407783a076046dff8cd593d8d59c2518a6b423
Tjelvar Guo <tjegu689@student.liu.se>
2018-11-26 20:01:01 +0100

    increase the strength range by one, align monkey to the right, remove underline by default if input

--- passwordstrengthmeter/src/main/java/com/mastega/passwordstrengthmeter/StockStrengthChecker.java
+++ passwordstrengthmeter/src/main/java/com/mastega/passwordstrengthmeter/StockStrengthChecker.java
@@ -18,46 +18,49 @@ public class StockStrengthChecker implements StrengthChecker{
         boolean foundSpecialChar= false;
         boolean foundDigit= false;
 
-        if (password.length() >= 8){
+        if (password.length() >= 1) {
             strength += 1;
-
-            if (password.length() >= 12) {
+            if (password.length() >= 8){
                 strength += 1;
-            }
 
-            for (int i = 0; i < password.length(); i++) {
-                if (foundUpperCase && foundLowerCase && foundSpecialChar && foundDigit) {
-                    break;
+                if (password.length() >= 12) {
+                    strength += 1;
                 }
 
-                Character character = password.charAt(i);
+                for (int i = 0; i < password.length(); i++) {
+                    if (foundUpperCase && foundLowerCase && foundSpecialChar && foundDigit) {
+                        break;
+                    }
 
-                if (Character.isDigit(character)) {
-                    foundDigit = true;
-                } else if (Character.isUpperCase(character)){
-                    foundUpperCase = true;
-                } else if (Character.isLowerCase(character)) {
-                    foundLowerCase = true;
-                } else {
-                    Pattern p = Pattern.compile("[^A-Za-z0-9]");
-                    Matcher m = p.matcher(password);
-                    if (m.find()) {
-                        foundSpecialChar = true;
+                    Character character = password.charAt(i);
+
+                    if (Character.isDigit(character)) {
+                        foundDigit = true;
+                    } else if (Character.isUpperCase(character)){
+                        foundUpperCase = true;
+                    } else if (Character.isLowerCase(character)) {
+                        foundLowerCase = true;
+                    } else {
+                        Pattern p = Pattern.compile("[^A-Za-z0-9]");
+                        Matcher m = p.matcher(password);
+                        if (m.find()) {
+                            foundSpecialChar = true;
+                        }
                     }
                 }
-            }
 
-            if (foundLowerCase && foundUpperCase) {
-                strength += 1;
-            }
-            if (foundDigit) {
-                strength += 1;
-            }
-            if (foundSpecialChar) {
-                strength += 1;
+                if (foundLowerCase && foundUpperCase) {
+                    strength += 1;
+                }
+                if (foundDigit) {
+                    strength += 1;
+                }
+                if (foundSpecialChar) {
+                    strength += 1;
+                }
             }
         }
-        
+
         return strength;
     }
 }
