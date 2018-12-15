75b3a2ce012658b5cf16c158711203cc3bd4e2f9
Tjelvar Guo <tjegu689@student.liu.se>
2018-12-15 13:55:56 +0100

    outline of state progress meter

--- progressmeter/src/main/java/com/mastega/progessmeter/ProgressMeter.java
+++ progressmeter/src/main/java/com/mastega/progessmeter/ProgressMeter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextSwitcher;
@@ -41,33 +43,45 @@ public class ProgressMeter extends CardView {
 
         for (int i = 0; i < 3; i++) {
             ImageView imageView = new ImageView(ctx);
-            imageView.setBackground(ctx.getDrawable(R.drawable.awesome));
+            imageView.setImageDrawable(ctx.getDrawable(R.drawable.awesome));
             stateIVs.add(imageView);
 
             TextView textView = new TextView(ctx);
             String stateText = "State " + i;
             textView.setText(stateText);
+            textView.setTextAlignment(TEXT_ALIGNMENT_CENTER);
             stateTVs.add(textView);
         }
 
+        LinearLayout.LayoutParams stateIVParams = new LinearLayout.LayoutParams(
+                dpAsPixels(25), dpAsPixels(25));
+        stateIVParams.weight = 1;
 
+        LinearLayout.LayoutParams stateTVParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        stateTVParams.weight = 1;
+
+        LinearLayout.LayoutParams verticalCenterParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        verticalCenterParams.gravity = Gravity.CENTER_VERTICAL;
+        
         LinearLayout container = new LinearLayout(ctx);
         container.setOrientation(LinearLayout.VERTICAL);
 
         LinearLayout statesLayout = new LinearLayout(ctx);
         statesLayout.setOrientation(LinearLayout.HORIZONTAL);
         for (ImageView imageView : stateIVs) {
-            statesLayout.addView(imageView);
+            statesLayout.addView(imageView, stateIVParams);
         }
         container.addView(statesLayout);
 
         LinearLayout statesTextLayout = new LinearLayout(ctx);
         statesTextLayout.setOrientation(LinearLayout.HORIZONTAL);
         for (TextView stateText : stateTVs) {
-            statesTextLayout.addView(stateText);
+            statesTextLayout.addView(stateText, stateTVParams);
         }
         container.addView(statesTextLayout);
-
+        this.addView(container, verticalCenterParams);
     }
 
     private int dpAsPixels (int dp) {
